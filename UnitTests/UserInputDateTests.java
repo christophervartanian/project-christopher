affdee070fdc73b5bafbda8830629de3d9dc4466
NoahWendland <npwendland@wustl.edu>
2022-04-11 18:32:29 -0500

    adding unit tests for validation

--- UnitTests/UserInputDateTests.java
+++ UnitTests/UserInputDateTests.java
@@ -3,32 +3,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import src.Date;
 import src.UserInputDate;
 
 public class UserInputDateTests {
-	@Test
-	void testUserInputDate20200404() throws FileNotFoundException {
-		UserInputDate userInput = new UserInputDate();
-		userInput.gatherUserInput();
-		Date d = userInput.getDateWeatherData();
-		assertTrue("20200404".equals(d.getDateString()));
-		assertTrue("69.0".equals(d.getPredLow()));
-		assertTrue("83.0".equals(d.getPredHigh()));
-		assertTrue("69.5".equals(d.getRealLow()));
-		assertTrue("82.4".equals(d.getRealHigh()));
+	
+	@BeforeEach 
+	void createUserInputDateObject() throws FileNotFoundException {
+		UserInputDate t = new UserInputDate();
 	}
+	
 	@Test
-	void testUserInputDate19770303() throws FileNotFoundException {
-		UserInputDate userInput = new UserInputDate();
-		userInput.gatherUserInput();
-		Date d = userInput.getDateWeatherData();
-		assertTrue("19770303".equals(d.getDateString()));
-		assertTrue("71.0".equals(d.getPredLow()));
-		assertTrue("86.0".equals(d.getPredHigh()));
-		assertTrue("67.4".equals(d.getRealLow()));
-		assertTrue("80.6".equals(d.getRealHigh()));
+	void testIsNumericFalse () {
+		U
+		boolean numeric = test.isNumeric("notnumeric");
+		
 	}
+	
+//	@Test
+//	void testUserInputDate20200404() throws FileNotFoundException {
+//		UserInputDate userInput = new UserInputDate();
+//		userInput.gatherUserInput();
+//		Date d = userInput.getDateWeatherData();
+//		assertTrue("20200404".equals(d.getDateString()));
+//		assertTrue("69.0".equals(d.getPredLow()));
+//		assertTrue("83.0".equals(d.getPredHigh()));
+//		assertTrue("69.5".equals(d.getRealLow()));
+//		assertTrue("82.4".equals(d.getRealHigh()));
+//	}
+//	@Test
+//	void testUserInputDate19770303() throws FileNotFoundException {
+//		UserInputDate userInput = new UserInputDate();
+//		userInput.gatherUserInput();
+//		Date d = userInput.getDateWeatherData();
+//		assertTrue("19770303".equals(d.getDateString()));
+//		assertTrue("71.0".equals(d.getPredLow()));
+//		assertTrue("86.0".equals(d.getPredHigh()));
+//		assertTrue("67.4".equals(d.getRealLow()));
+//		assertTrue("80.6".equals(d.getRealHigh()));
+//	}
 }
