2b22c20b6b42d1ab84a343b05a607fd59206d59b
NoahWendland <npwendland@wustl.edu>
2022-04-11 16:40:03 -0500

    fixed bugs with input date validation

--- src/UserInputDate.java
+++ src/UserInputDate.java
@@ -34,29 +34,60 @@ public class UserInputDate {
 	public void checkInputValidFormat() {
 		while (validInputDate == false) {
 			String chosenDate = inputDateScanner.nextLine();
-			if (chosenDate.length() == 8) {
+			boolean inputIsNumeric = isNumeric(chosenDate);
+			if (chosenDate.length() == 8 && inputIsNumeric ) {
 				validInputDate = checkInputInValidDateRange(chosenDate);
 			} else {
 				System.out.println("Please enter a valid date");
 			}
 		}
 	}
+	public boolean isNumeric (String strToCheck) {
+		boolean numeric = false;
+		try {  
+		    Integer.parseInt(strToCheck);  
+		    numeric = true;
+		  } catch(NumberFormatException e){  
+		    numeric = false;
+		  }  
+		return numeric;
+	}
 
 	public boolean checkInputInValidDateRange(String chosenDate) {
-		int year = Integer.parseInt(chosenDate.substring(0, 4));
-		int month = Integer.parseInt(chosenDate.substring(4, 6));
-		int day = Integer.parseInt(chosenDate.substring(6));
-		if ((year < 1940) || (year > 2026)) {
-			System.out.println("Please enter a valid date");
-		} else if ((month < 01) || (month > 12)) {
-			System.out.println("Please enter a valid date");
-		} else if ((day < 01) || (day > 31)) {
-			System.out.println("Please enter a valid date");
-		} else {
+		int[] YYYYMMDD = convertDateStringToYYYYMMDD(chosenDate);
+		boolean isValidDate = checkValidDateRange(YYYYMMDD);
+		if (isValidDate) {
 			validInputDate = true;
 			dateWeatherData.setDateString(chosenDate);
 		}
+		else {
+			System.out.println("Please enter a valid date");
+		}
 		return validInputDate;
+	}
+	
+	public int[] convertDateStringToYYYYMMDD (String chosenDate) {
+		int[] YYYYMMDD = new int[3];
+		int year = Integer.parseInt(chosenDate.substring(0, 4));
+		int month = Integer.parseInt(chosenDate.substring(4, 6));
+		int day = Integer.parseInt(chosenDate.substring(6));
+		YYYYMMDD[0] = (year);
+		YYYYMMDD[1] = (month);
+		YYYYMMDD[2] = (day);
+		return YYYYMMDD;
+		
+	}
+	
+	public boolean checkValidDateRange (int[] YYYYMMDD) {
+		boolean isValidDate = true;
+		if ((YYYYMMDD[0] < 1940) || (YYYYMMDD[0] > 2026)) {
+			isValidDate = false;
+		} else if ((YYYYMMDD[1] < 01) || (YYYYMMDD[1] > 12)) {
+			isValidDate = false;
+		} else if ((YYYYMMDD[2] < 01) || (YYYYMMDD[2] > 31)) {
+			isValidDate = false;
+		} 
+		return isValidDate;
 	}
 
 	public void setDateWeatherData() {
@@ -79,5 +110,6 @@ public class UserInputDate {
 		System.out.println("Predicted Temperature Low: " + dateWeatherData.getRealLow());
 		System.out.println("Predicted Temperature High: " + dateWeatherData.getRealHigh());
 	}
+
 }
 
