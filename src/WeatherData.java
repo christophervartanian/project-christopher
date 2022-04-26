f3319f67ebc64f2dacc52a2656502a6eabf1932f
christopherjohnvartanian <christopherjohnvartanian@192.168.0.14>
2022-04-26 17:35:35 -0500

    Updated outputs

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -3,8 +3,24 @@ package src;
 import java.io.FileNotFoundException;
 
 public class WeatherData {
+	
+	 
 	 public static void main(String[] args) throws FileNotFoundException {
+		 
+		 System.out.println("Hello");
 		 UserInputDate userInput = new UserInputDate();
 		 userInput.gatherUserInput();
+		 userInput.searchInCSVAndSetValues();
+		 System.out.println();
+		 System.out.println("Weather Report:");
+		 userInput.printDateWeatherData();
+		 System.out.println();
+		 System.out.println("Breakdown:");
+		 userInput.precipBreakdown();
+		 userInput.tempBreakdown();
+		 
+
+	
+		 
 	 }
 }
